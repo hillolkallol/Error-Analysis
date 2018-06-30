6b22f3b7082154289d92308a0bf8cbbace5183b8
kallol <kalloldash@gmail.com>
2018-06-29 22:22:14 -0600

    Calculating RMSE for 70 percentage

--- RMSEFor_70Day_25x25_SD.py
+++ RMSEFor_70Day_25x25_SD.py
@@ -1,10 +1,14 @@
 import numpy as np
 import csv
 from netCDF4 import Dataset
+from sklearn.metrics import *
+from numpy import *
 
 #reading netcdf
 netcdf_entire_dataset = Dataset("F:/dataset/rain_data/summing_dataset.nc", "r")
 rain_models = netcdf_entire_dataset.variables['summing_models']
+days_error_rate_file = netcdf_entire_dataset.variables['days'][:]
+time_error_rate_file = netcdf_entire_dataset.variables['time'][:]
 models_error_rate_file = netcdf_entire_dataset.variables['models'][:]
 
 with open('random70.csv') as csvf:
@@ -15,8 +19,9 @@ with open('random70.csv') as csvf:
 np.set_printoptions(formatter={'float': '{: 0.4f}'.format})
 np.seterr(divide='ignore', invalid='ignore')
 
+
 #creating csv file
-check = open('MAE_70_25x25.csv', 'w')
+check = open('RMSE_70_25x25.csv', 'w')
 check.truncate()
 # writing the headers
 check.write(str('Y'))
@@ -44,32 +49,43 @@ for y in range(46): # 46 y-coordinates
             original_data = []
             rain100 = []
             for i in index70:
-                original_data.append(np.array(rain_models[i, :10, 0, y, x])) # real data
-                rain100.append(np.array(rain_models[i, :10, i, y, x])) # model data
+                original_data.append(np.array(rain_models[i, :10, 0, y, x]))  # real data
+                rain100.append(np.array(rain_models[i, :10, i, y, x]))  # model data
+            # rain100[rain100>30000] = np.nan
 
-            a = abs(np.array(original_data) - np.array(rain100)) # taking the absolute value
+            # print(sqrt(power(abs(original_data - rain100), 2)))
+            # print(original_data)
+            # print(rain100)
+            a = np.array(power((np.array(original_data) - np.array(rain100)), 2)) # square of the difference
+            # print(a[0, 0])
+
             # print(len(a), len(a[0]))
             # print(a[2,3])
             # print(np.nanmin(a), np.nanmax(a))
             # a[a > 30000] = np.nan
-            if not np.isnan(a).all():
+            if not (np.isnan(a).all() and np.isinf(a).all):
+
                 # print('yes')
                 # print(MAE[j, k, i, :, :])
                 # sum = sum + np.array(a)
                 # print(sum)
                 countArr = countArr + 1 #counting for all values
 
-                mask = ((np.array(original_data) == 0) & (np.array(rain100) == 0)) # if both real and prediction model has value zero
+                mask = ((np.array(original_data) == 0) & (np.array(rain100) == 0)) | (a == np.inf) | (a == np.nan) # if both real and prediction model has value zero
                 # print('mask found:', mask)
                 countArr[mask] = countArr[mask] - 1 # doing (-1) for not counting zero values
-                max = np.round(np.nanmax(a), 4) # maximum value! not using anymore
-                min = np.round(np.nanmin(a), 4)
+                # max = np.round(np.nanmax(a), 4)
+                # min = np.round(np.nanmin(a), 4)
                 # print(min,max)
 
                 a[a == np.nan] = 0
-                sum = np.nansum(a) # summing all non-nan values
-                count= np.sum(countArr) # summing all counts
-                avg = sum / count # this is the MAE
+                a[a == np.inf] = 0
+
+                # print(a)
+                sum = np.nansum(a) #summing all non-nan values
+                    # print(sum)
+                count= np.sum(countArr)
+                avg = sqrt(sum / count) # root mean square error
 
                 check.write(str(avg))
                 check.write(', ')
